b4d2e844aff1f63d2f16c74b8647382fa470a5bb
Lorenzo Zafra <zafra@ualberta.ca>
2019-04-30 21:38:21 -0600

    Fix bug

--- scripts/install.sh
+++ scripts/install.sh
@@ -43,4 +43,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+zsh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
